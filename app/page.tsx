3504df1b690b290bc23e19d7188722c6ce0d5e2f
Shashwat Raj <rajayshashwat@gmail.com>
2025-03-01 13:09:15 -0700

    bio added

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,9 @@ export default function Page() {
         Hello There! I'm Shashwat Raj.
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I am a sophomore majoring in Computer Systems Engineering  with a minor in Mathematics at Arizona State University. 
+        
+        I am a passionate software developer and a tech enthusiast. I love to learn new technologies and work on projects that challenge me. I am currently working on a project that involves building a web application using React, TypeScript, and Firebase. I am also learning about cloud computing and machine learning. I am looking for internships in software development and software engineering roles. I am open to learning new technologies and working on projects that challenge me.`}
       </p>
       <div className="my-8">
         <BlogPosts />
