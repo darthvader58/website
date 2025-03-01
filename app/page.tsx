d2d1601a733dcf0c47e8d1db8ecd9e766b7b1f2f
Shashwat Raj <rajayshashwat@gmail.com>
2025-03-01 14:44:15 -0700

    Added resume, updated bio

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,8 @@ export default function Page() {
         Hello There! I'm Shashwat Raj.
       </h1>
       <p className="mb-4">
-        {`I am a sophomore majoring in Computer Systems Engineering  with a minor in Mathematics at Arizona State University. 
-        
-        I am a passionate software developer and a tech enthusiast. I love to learn new technologies and work on projects that challenge me. I am currently working on a project that involves building a web application using React, TypeScript, and Firebase. I am also learning about cloud computing and machine learning. I am looking for internships in software development and software engineering roles. I am open to learning new technologies and working on projects that challenge me.`}
+        {`I am a sophomore majoring in Computer Systems Engineering  with a minor in Mathematics at Arizona State University. \n
+        I am passionate about math, robotics and software development. I love to learn new technologies and work around Machine Learning projects. Currently being an undergraduate student researcher, I am developing reinforcement learnings for optimising Earth Science Observations for autonomously determining priority observations in a dynamic environment. I am also learning about cloud computing and languages like Go and Cuda. I am also a founder of 2 startups and host of a podcast - Write It Out. \n Checkout my startup WoofCare and some of my other popular projects - Formath, Terrader, Whatrobe and Tansen.`}
       </p>
       <div className="my-8">
         <BlogPosts />
