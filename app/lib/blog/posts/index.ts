4ba3fdef006e6ba66e1953007283f61ad553e2da
Shashwat Raj <rajayshashwat@gmail.com>
2026-05-17 03:36:46 -0700

    second blog

--- app/lib/blog/posts/index.ts
+++ app/lib/blog/posts/index.ts
@@ -1,4 +1,8 @@
 import { theBlackBoxHasAGroupChatPost } from './001-the-black-box-has-a-group-chat'
+import { theTransformerMomentAndQuantumsSearchForArchitecturePost } from './002-the-transformer-moment-and-quantums-search-for-architecture'
 
 // Add each new blog post file here and give it a matching folder in public/blog-images/<post-number>.
-export const blogPosts = [theBlackBoxHasAGroupChatPost]
+export const blogPosts = [
+  theBlackBoxHasAGroupChatPost,
+  theTransformerMomentAndQuantumsSearchForArchitecturePost,
+]
